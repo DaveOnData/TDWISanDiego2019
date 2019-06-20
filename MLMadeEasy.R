7b0850a5d09da1d8f58fb83ffde19d6acd137c63
David Langer <52019807+DaveOnData@users.noreply.github.com>
2019-06-19 20:37:55 -0600

    Initial code commit.

---
+++ MLMadeEasy.R
@@ -0,0 +1,103 @@
+#=======================================================================================
+#
+# File:        MLMadeEasy.R
+# Authors:     Caitlin Johnson & Dave Langer
+# Description: This code was written as part of the 1-day course "Hands-on: Machine 
+#              Learning Made Easy - No, Really!" delivered at the August 2019 TDWI 
+#              Sand Diego Conference. Additional materials for the course can
+#              be found at:
+#
+#                 https://github.com/DaveOnData/TDWISanDiego2019
+#
+#              The code in this file leverages a cleaned version of the UCI ML 
+#              Repository's "Adult" dataset. The original data is available at:
+#
+#                 http://archive.ics.uci.edu/ml/datasets/Adult
+#
+# NOTE - This file is provided "As-Is" and no warranty regardings its contents are
+#        offered nor implied. USE AT YOUR OWN RISK!
+#
+#=======================================================================================
+
+
+
+#=======================================================================================
+#
+# Section 2 - Introduction to decision trees
+#
+#=======================================================================================
+
+# Load the libraries we will use for this section of the course
+library(rpart)
+library(rpart.plot)
+
+
+# NOTE - Set your working directory before runing the following.
+# Re-load adult training data.
+adult.train <- read.csv("AdultTrain.csv")
+
+
+
+
+#=======================================================================================
+#
+# Section 3 - Introduction to random forests
+#
+#=======================================================================================
+
+# Load the libraries we will use for this section of the course
+library(randomForest)
+
+
+# NOTE - Set your working directory before runing the following.
+# Re-load adult training data.
+adult.train <- read.csv("AdultTrain.csv")
+
+
+# Let's take a look at the help file.
+?randomForest
+
+
+# Time to train our first random forest. Ask the algorithm to predict our
+# label using all the available features usign R's formula syntax.
+first.rf <- randomForest(Label ~ ., data = adult.train)
+
+
+# The randomForest function gives us a lot of info back.
+first.rf
+
+
+
+#=======================================================================================
+#
+# Section 4 - Data analysis & feature engineering with random forests
+#
+#=======================================================================================
+
+
+
+
+
+
+
+#=======================================================================================
+#
+# Section 5 - Introduction to predictive model testing
+#
+#=======================================================================================
+
+# Load the libraries we will use for this section of the course
+library(randomForest)
+
+
+# NOTE - Set your working directory before runing the following.
+# Re-load adult training data.
+adult.train <- read.csv("AdultTrain.csv")
+
+# Load adult test data.
+adult.test <- read.csv("AdultTest.csv")
+
+
+
+
+
