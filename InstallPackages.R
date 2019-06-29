f0538793296a9306ab18e9163448428dedef7154
David Langer <david.langer@gmail.com>
2019-06-29 14:36:47 -0600

    Adding Files
    InstallPackages.R
    LaptopPrepMLMadeEasy.pdf

---
+++ InstallPackages.R
@@ -0,0 +1,22 @@
+#=======================================================================================
+#
+# File:        InstallPackages.R
+# Authors:     Caitlin Johnson & Dave Langer
+# Description: This code was written as part of the 1-day course "Hands-on: Machine 
+#              Learning Made Easy - No, Really!" delivered at the August 2019 TDWI 
+#              San Diego Conference. Additional materials for the course can
+#              be found at:
+#
+#                 https://github.com/DaveOnData/TDWISanDiego2019
+#
+# NOTE - This file is provided "As-Is" and no warranty regarding its contents is
+#        offered nor implied. USE AT YOUR OWN RISK!
+#
+#=======================================================================================
+
+
+# Install packages required for the course.
+install.packages(c("rpart", "rpart.plot", "randomForest", "caret"),
+                 dependencies = TRUE)
+
+
